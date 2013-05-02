050156a89957626cdbeff37c3634ab1575fa4ce7
Kristofor Carle <kristofor.carle@syncadd.com>
2013-05-02 16:23:25 -0400

    initial yeoman 1.0 app

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('ofpViewerApp', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ofpViewerApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
