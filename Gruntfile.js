22eed4485c17e525cf7ba9eec5e33b525ba8a027
Kristofor Carle <kristofor.carle@syncadd.com>
2015-01-17 20:21:43 -0500

    Misc build and IDEA project updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -264,8 +264,8 @@ module.exports = function (grunt) {
 
   grunt.registerTask('server', [
     'clean:server',
-    'coffee:dist',
-    'compass:server',
+    //'coffee:dist',
+    //'compass:server',
     'livereload-start',
     'connect:livereload',
     'open',
